b5e1013b0cb25ce2d009d7f87ea2f741f2064428
yzheng21 <zhengyufan94@gmail.com>
2020-02-03 21:14:40 -0800

    complete the album and artist view

--- src/actions/index.js
+++ src/actions/index.js
@@ -52,3 +52,92 @@ export const fetchArtists = () => {
          })
    }
 }
+
+export const fetchArtist = artist => {
+   return dispatch => {
+      return Api.fetchArtist(artist)
+         .then(albums => {
+            const albumData = {};
+
+            for (let i in albums) {
+               const album = albums[i];
+               albumData[album.album] = [];
+            }
+
+            dispatch({
+               type: 'FETCH_ARTIST_SUCCESS',
+               name: artist,
+               albums,
+               albumData,
+            });
+         })
+         .catch(error => {
+            console.log(error);
+         });
+   };
+};
+
+export const fetchAlbums = () => {
+   return dispatch => {
+      return Api.fetchAlbums()
+         .then(albums => {
+            const albumData = {};
+
+            for (let album of albums) {
+               const name = album.album;
+               albumData[name] = [];
+            }
+
+            dispatch({
+               type: 'FETCH_ALBUM_LIST_SUCCESS',
+               albumData,
+               albums,
+            });
+         })
+         .catch(error => {
+            console.log(error);
+         });
+   };
+};
+
+export const fetchAlbum = ({ artist, album }) => {
+   return dispatch => {
+      return Api.fetchAlbum({ artist, album })
+         .then(tracks => {
+            dispatch({
+               type: 'FETCH_ALBUM_SUCCESS',
+               album,
+               tracks,
+            });
+         })
+         .catch(error => {
+            console.log(error);
+         });
+   };
+};
+
+export const addToPlaylist = (track, playlist) => {
+   return dispatch => {
+      let playlists = localStorage.appleMusicPlaylists;
+      playlists = playlists ? JSON.parse(playlists) : playlists;
+
+      // Add track to playlist
+      playlist = {
+         ...playlist,
+         tracks: [...playlist.tracks, track],
+      };
+
+      // Update playlist in playlist list
+      playlists = {
+         ...playlists,
+         [playlist.title]: playlist,
+      };
+
+      localStorage.appleMusicPlaylists = JSON.stringify(playlists);
+
+      dispatch({
+         type: 'UPDATE_PLAYLIST',
+         playlists,
+      });
+   };
+};
